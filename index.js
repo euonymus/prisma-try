1b238700287aeed8dc5cc3884da62cccd54dbc1b
euonymus <euonymus0220@gmail.com>
2018-11-28 17:31:01 +0900

    Tutorial: Change Datamodel

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const { prisma } = require('./generated/prisma-client')
 // A `main` function so that we can use async/await
 async function main() {
 
-  // Create a new user called 'Alice'
-  const newUser = await prisma.createUser({ name: 'Alice' })
+  // Create a new user with a new post
+  const newUser = await prisma
+    .createUser({
+      name: "Bob",
+      email: "bob@prisma.io",
+      posts: {
+        create: {
+          title: "The data layer for modern apps",
+        }
+      },
+    })
   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
 
   // Read all users from the database and print them to the console
   const allUsers = await prisma.users()
   console.log(allUsers)
+
+  const allPosts = await prisma.posts()
+  console.log(allPosts)
 }
 
 main().catch(e => console.error(e))
